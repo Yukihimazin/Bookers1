6d99cff9da217a35bc6eeca73cc9687773713b2e
Yukihimazin <95333923+Yukihimazin@users.noreply.github.com>
2023-10-28 14:30:37 +0000

    Second Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,54 +1,52 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new
+
+  end
 
   def create
-    #データを受け取り新規登録するためのインスタンス作成
     @book = Book.new(book_params)
-    #データをデータベースに保存するためのsaveメソッド実行
+
     if @book.save
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
-      render 'index'
+      render :index
     end
-
-  end
-
-  def index
-    @books = Book.all
-    @book= Book.new
-  end
-
-  def show
-    @book = Book.find(params[:id])
   end
 
   def edit
     @book = Book.find(params[:id])
   end
 
-  def update
+  def show
     @book = Book.find(params[:id])
+  end
 
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
 
-
+  def update
+   @book = Book.find(params[:id])
     if @book.update(book_params)
-      flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(@book.id)
+       flash[:notice] = "Book was successfully updated."
+       redirect_to book_path(@book.id)
     else
-      render :edit
+      @books = Book.all
+      render :show
     end
-
-
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to'/books'
+    redirect_to "/books"
   end
 
-  private
+private
   def book_params
     params.require(:book).permit(:title, :body)
   end

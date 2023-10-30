4f9dc6438f1e6c2f90390a44eb046c680615a990
Yukihimazin <95333923+Yukihimazin@users.noreply.github.com>
2023-10-30 15:28:58 +0000

    third commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,15 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-
-  end
-
   def create
     @book = Book.new(book_params)
-
     if @book.save
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      @books = Book.all
+      @books =Book.all
       render :index
     end
   end
+
 
   def edit
     @book = Book.find(params[:id])
@@ -36,14 +31,15 @@ class BooksController < ApplicationController
        redirect_to book_path(@book.id)
     else
       @books = Book.all
-      render :show
+      render :edit
     end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to "/books"
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to '/books'
   end
 
 private
